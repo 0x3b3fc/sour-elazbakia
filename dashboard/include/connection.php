d34f1b0803ea1e64b9331b9cd8dbef0b0bd2dcbb
0x3b3fc <dev.sief.hesham@gmail.com>
2022-09-24 04:44:38 +0200

    sour alazbakia free book store

---
+++ dashboard/include/connection.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbName = 'azbakia_db';
+
+$con = mysqli_connect($host, $user, $pass, $dbName);
---
+++ layout/include/footer.php
@@ -0,0 +1,19 @@
+    <footer class="text-center">
+        <p>جميع الحقوق محفوظة &copy; 2022</p>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- jQuery -->
+    <script src="layout/js/jquery-3.4.1.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="layout/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("body").on("contextmenu", function(e) {
+                return false;
+            });
+        });
+    </script>
+    </body>
+
+    </html>
